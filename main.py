87acd3b49c09feba559bac65e95a1e760687a3ac
khnshn <programming.khanshan@gmail.com>
2023-06-16 11:56:24 +0200

    update system message

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ EXTENTION = ".py"
 PROMPT_HEAD = [
     {
         "role": "system",
-        "content": "You are a helpful assistant that generates python code per request",
+        "content": "You are a helpful assistant that generates python code per request. Your output only includes executable source code. Your output does not include any sort of explanation nor description",
     }
 ]
 
